e47b2733e0dfd33e5908a1ed2d463658654652d0
tangentstorm <michal.wallace@gmail.com>
2022-04-02 22:59:49 -0400

    raco pkg skeleton

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "k3")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/k3.scrbl" ())))
+(define pkg-desc "k3 language support for racket")
+(define version "0.0")
+(define pkg-authors '(tangentstorm))
+(define license '(MIT))
---
+++ scribblings/k3.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[k3
+                    racket/base]]
+
+@title{k3}
+@author{tange}
+
+@defmodule[k3]
+
+Package Description Here
