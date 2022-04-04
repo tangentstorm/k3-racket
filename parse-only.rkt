f94fa7d00959c9598c84355e998dd1235634eae8
tangentstorm <michal.wallace@gmail.com>
2022-04-04 18:16:36 -0400

    syntax highlighting for #lang k3/parse-only

--- parse-only.rkt
+++ parse-only.rkt
@@ -2,9 +2,9 @@
 ; #lang k3/parse-only
 ; provides a reader and expander that
 ; simply shows the parse tree
-(require "parser.rkt" "lexer.rkt")
+(require "parser.rkt" "lexer.rkt" "main.rkt")
 (provide (rename-out [parser-only-mb #%module-begin]))
-(module+ reader (provide read-syntax))
+(module+ reader (provide read-syntax get-info))
 
 (define (read-syntax path port)
   (strip-bindings
@@ -15,3 +15,10 @@
   #'(#%module-begin
      'PARSE-TREE))
 
+(define (get-info port src-mod src-line src-col src-pos)
+  (define (handle-query key default)
+    (case key
+      [(color-lexer)
+       (dynamic-require 'k3/color 'k3-color)]
+      [else default]))
+  handle-query)
