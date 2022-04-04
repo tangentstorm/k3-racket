9491e77bf080dc2048d734fe457d82171bfcd053
tangentstorm <michal.wallace@gmail.com>
2022-04-04 15:11:04 -0400

    splice k-stmt-line nodes but not k-expr nodes

--- expand.rkt
+++ expand.rkt
@@ -3,17 +3,19 @@
 ; k3 expander
 ; this supplies the functions and macros that expand
 ; the parsed source k3 code into a racket expression.
+;
+; there should be one definition here for every rule
+; in parser.rkt that does not have a '@' in front of the name.
+; (the '@' indicates that it gets inlined)
+
 (provide (rename-out [k3-module-begin #%module-begin])
-         k-line k-endnote)
+         (matching-identifiers-out #rx"^k-" (all-defined-out)))
 
 (define-macro (k3-module-begin (k-code K3 ...))
   #'(#%module-begin K3 ...))
 
-#;(define-macro (k-comment ARG)
-  #'(display ARG))
+(define (k-comment ARG) (void))
+(define-macro (k-endnote ARG) (void))
 
-(define-macro (k-line ARG ...)
-  #'(display #'(ARG ...)))
-
-(define-macro (k-endnote ARG)
-  #'(display ARG))
+(define-macro (k-expr ARG ...)
+  #'(displayln '(k-expr ARG ...)))
--- parser.rkt
+++ parser.rkt
@@ -8,8 +8,8 @@ k-endnote : APPENDIX
 @k-line : k-comment-line | k-stmt-line | k-meta-line | /NL
 k-meta-line : "\\" .* /NL
 @k-comment-line : k-comment /NL
-k-stmt-line : k-expr k-comment? /NL
+@k-stmt-line : k-expr k-comment? /NL
 k-comment : COMMENT
-@k-expr : (LNAME|PRIM|ADVERB|k-func|NUMBER|STRING|SYMBOL|":"|";")+
+k-expr : (LNAME|PRIM|ADVERB|k-func|NUMBER|STRING|SYMBOL|":"|";")+
 @k-block: k-line* k-expr?
 k-func : "{" k-block "}"
