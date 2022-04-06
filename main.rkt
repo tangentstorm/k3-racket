8a7c42e5116d4b87e91243c860135a87ae1a2df4
tangentstorm <michal.wallace@gmail.com>
2022-04-06 19:55:54 -0400

    main: provide read-k3 (like read-syntax but no module wrapper)

--- main.rkt
+++ main.rkt
@@ -4,14 +4,19 @@
 ;
 (require brag/support
          "lexer.rkt" "parser.rkt")
+(provide read-k3)
 
 (module+ reader
   (provide read read-syntax get-info))
 
+(define (read-k3 path port)
+  (strip-bindings
+   (parse path (make-k3-lexer port path))))
+
 (define (read-syntax path port)
   (strip-bindings
    #`(module k3-module k3/expand
-       #,(parse path (make-k3-lexer port path)))))
+       #,(read-k3 path port))))
 
 (define (read port)
   (syntax->datum (read-syntax "" port)))
