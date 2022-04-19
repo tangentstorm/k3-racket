a4332bc5b5335618b89bb974add4d7628357e148
tangentstorm <michal.wallace@gmail.com>
2022-04-19 12:46:13 -0400

    editor: clicking token prints context from parse tree

--- editor.rkt
+++ editor.rkt
@@ -1,6 +1,31 @@
 #lang racket/gui
 (require framework)
-(require k3/color)
+(require k3/color k3)
+(require syntax/stx)
+
+
+(define k-path "example.k")
+
+; --  syntax tree support  ----------------------------------------
+
+(define (pos-in-syntax? pos stx)
+  (let* [(s-pos (syntax-position stx))
+         (s-end (+ s-pos (syntax-span stx)))]
+    (and (<= s-pos pos)
+         (< pos s-end))))
+
+; given a position, find a token and all its parents in the syntax tree.
+(define (find-token stx pos)
+  (define (find-token-rev stx pos)
+    (if (stx-list? stx)
+        (let [(f (findf (curry pos-in-syntax? pos)
+                        (rest (syntax-e stx))))]
+          (cons stx (find-token-rev f pos)))
+        (list stx)))
+  (reverse (find-token-rev stx pos)))
+
+
+; -- syntax colors  -----------------------------------------
 
 (define (find-color c) (send the-color-database find-color c))
 ;(editor:set-default-font-color (find-color "Red") (find-color "Yellow"))
@@ -9,10 +34,8 @@
 (define Syntax (send styles new-named-style "Syntax" (send styles basic-style)))
 (let [(delta (new style-delta%))]
   (send delta set-delta-face "Consolas")
-  ;(send delta set-delta-background "#111122")
   (send delta set-delta 'change-size 12)
   (send Syntax set-delta delta))
-
 
 (define (add-style name color)
   (let ([new-style (send styles new-named-style name Syntax)]
@@ -22,29 +45,45 @@
     (send new-style set-delta delta)
     new-style))
 
-(add-style "string" "Olive")
-(add-style "symbol" "Dark Goldenrod")
-(add-style "constant" "Steel Blue")
-(add-style "comment" "Dark Magenta")
-(add-style "keyword" "Dark Green")
-(add-style "hash-colon-keyword" "Peru")
-(add-style "parenthesis" "Dark Slate Gray")
-(add-style "other" "Magenta")
-(add-style "Standard" "Black")
+(void
+ (add-style "string" "Olive")
+ (add-style "symbol" "Dark Goldenrod")
+ (add-style "constant" "Steel Blue")
+ (add-style "comment" "Dark Magenta")
+ (add-style "keyword" "Dark Green")
+ (add-style "hash-colon-keyword" "Peru")
+ (add-style "parenthesis" "Dark Slate Gray")
+ (add-style "other" "Magenta")
+ (add-style "Standard" "Black"))
 
-(define k-path "example.k")
+(define k3-text%
+  (class color:text%
+    (super-new)
+
+    (define/override (on-local-event e)
+      (super on-local-event e)
+      (define ex (send e get-x))
+      (define ey (send e get-y))
+      (define-values (edx edy) (send this dc-location-to-editor-location ex ey))
+      (define pos (send this find-position edx edy))
+      (define ch (send this get-character pos))
+      (define snip (send this find-snip pos 'after))
+      (when (eq? 'left-down (send e get-event-type))
+        (printf "click @ pos:~a ch:~a snip:~a\n" pos ch snip)
+        (for [(node (find-token ast pos))] (displayln node))))))
+
+
 (define win (new frame% [label k-path] [width 1024] [height 768]))
 (define ed (new editor-canvas% (parent win)))
-(define txt (new color:text%))
+(define txt (new k3-text%))
 (send ed set-editor txt)
-(send txt load-file k-path)
-(send txt set-style-list styles)
-
-(define (token-sym->style sym)
-  (symbol->string sym))
+(define (token-sym->style sym) (symbol->string sym))
 (define pairs '((|(| |)|) (|[| |]|) (|{| |}|)))
 (send txt start-colorer token-sym->style k3-color pairs)
 
+; -- main program ------------------------------------------------
 
+(void (send txt load-file k-path))
+(send txt set-style-list styles)
 (send win show #t)
-
+(define ast (read-k3 k-path (open-input-string (send txt get-text 0 'eof))))
